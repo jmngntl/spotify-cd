1799f1b1e40884c8f9387c4adb8aaae14ac9a1af
Jalen Mangantulao <jalen.mangantulao@gmail.com>
2025-08-21 22:12:43 -0400

    create get class function + error handling for bad requests

--- scd/apps/connection/connect.py
+++ scd/apps/connection/connect.py
@@ -1,13 +1,15 @@
 import collections
 import logging
 import secrets
+import httpx
 
+from datetime import datetime, timedelta
 from django.conf import settings
 from authlib.integrations.httpx_client import OAuth2Client
 from requests.auth import HTTPBasicAuth
 
 # configure logging for this module
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('apiLogger')
 
 class SpotifyConnection():
     def __init__(self):
@@ -25,7 +27,7 @@ class SpotifyConnection():
         self.headers = {'Content-Type': 'application/x-www-form-urlencoded'}
         self.auth_info = HTTPBasicAuth(self.client_id, self.client_secret)
         self.code_verifier = self.generate_code_verifier()
-        self.login()
+        self.client = self.login()
 
     @staticmethod
     def generate_code_verifier():
@@ -39,6 +41,7 @@ class SpotifyConnection():
         """
         # TODO: in view where API is called and token is used use whlie condition to check if token has expired -- while response.status_code == httpx.codes.OK .. look into .elapsed attr from httpx docs could set OR condition in while to check if .elapsed < token keep alive time, if not refersh token
         # TODO: look into Spotify API docs for expires_at in access token response.. is this in UTC? 
+        # TODO: supress stdout when calling property
         if self._token is None:
             self._token = self.token_attrs.access_token
         return self._token
@@ -49,7 +52,7 @@ class SpotifyConnection():
         """
         return collections.namedtuple('TokenAttributes', token_data.keys())(**token_data)
 
-    def set_ouath2_client(self):
+    def initialize_client(self):
         """
         Set up OAuth2 clients with required Spotify API authentication parameters.
         """
@@ -58,32 +61,63 @@ class SpotifyConnection():
             client_secret=self.client_secret,
             redirect_uri=self.redirect_uri,
             scope=self.scope,
+            token_placement='header'
         )
         return spotify_client
+    
+    # def set_oauth2_client(self):
+    #     spotify_client = OAuth2Client(
+    #         client_id=self.client_id,
+    #         client_secret=self.client_secret,
+    #         redirect_uri=self.redirect_uri,
+    #         scope=self.scope,
+    #         token=self.token
+    #     )
+    #     return spotify_client
 
     def login(self):
         """
         Authorize access to user spotify data using OAuth2.
         """
-        spotify_client = self.set_ouath2_client()
+        spotify_client = self.initialize_client()
         try:
             # Authorize access to user data
             auth_resp, auth_state = spotify_client.create_authorization_url(
                 url=self.auth_url,
                 code_verifier=self.code_verifier  # use PKCE
             )
-            logger.info(f'Authorization URL: {auth_resp}')
-            logger.info('Please complete the authorization in your browser.')
 
             # Fetch the access token
             token_data = spotify_client.fetch_token(
                 url=self.token_url,
                 auth=self.auth_info,
-                headers=self.headers
+                authorization_response=auth_resp,
+                state=auth_state,
             )
-            logger.info(f'Token data fetched successfully: {token_data}')
+            # current_time = datetime.now()
+            # expiration_time = current_time + timedelta(seconds=token_data.get('expires_in'))
+            # if current_time < expiration_time:
+            #     token_data['is_expired'] = False
+            if token_data:
+                logger.info(f'Token data fetched successfully: {token_data}')
             self.token_attrs = self.get_token_attrs(token_data)
-            return self.token_attrs
+            spotify_client.token = self.token_attrs.access_token
+            return spotify_client
         except Exception as e:
             logger.error(f'Error during login: {e}')
+            raise e
+        
+    def get(self, url: str, **kwargs):
+        self.headers.update({'Authorization': f'Bearer {self.token}'})
+        logger.debug(f'GET - {url}')
+        try:
+            request = httpx.Request('GET', url, **kwargs, headers=self.headers)
+            response = self.client.send(request)
+            if response.status_code == httpx.codes.OK:
+                return response.json()
+            else:
+                logger.error(f'Error fetching data from {url}: {response.status_code} - {response.text}')
+                return response
+        except httpx.HTTPError as e:
+            logger.error(f'Invalid HTTP response: {e}')
             raise e
