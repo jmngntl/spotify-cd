6e63d701298539cc1742ea3fe3f53484ae27eddf
Jalen Mangantulao <jalen.mangantulao@gmail.com>
2025-09-18 21:48:38 -0400

    create authenticate view that starts auth session

--- scd/apps/connection/views.py
+++ scd/apps/connection/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+import logging
+
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from apps.connection.connect import SpotifyConnection
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
+def callback(request):
+    return render(request, '')
+
+
+def authenticate(request):
+    logger.debug("Starting auth process")
+    session = SpotifyConnection()
+    session_client = session.client
+    oauth = session.auth_popup
+    
+    context = {
+        'oauth_html': oauth
+    }
+    return render(request, 'connect.html', context)
