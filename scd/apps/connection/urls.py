fc3185754a6027d6f7347e252db566798fe94e01
Jalen Mangantulao <jalen.mangantulao@gmail.com>
2025-10-09 20:48:32 -0400

    remove deprecated authenticate view url

--- scd/apps/connection/urls.py
+++ scd/apps/connection/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.authenticate, name='authenticate'),
     path('redirect/', views.auth_redirect, name='auth_redirect'),
 ]
